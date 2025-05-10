c1f77e2cee9a338f562e283e2634e04908a631b5
S. Sherratt <sadie.sherratt@gmail.com>
2025-05-09 21:53:51 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-DELTA_CURVE_FILE = "../data/delta_curve.npy"
-DYNAMIC_SINE_ENVELOPE_FILE = "../data/dynamic_sine_envelope.npy"
-WITHIN_BAND_MASK_FILE = "../data/within_band_mask.npy"
-KNOWN_ZEROS_FILE = "../data/zeta_zeros.npy"
-
-# Sieve Parameters
+# Core sieve parameters
+BASE_FREQUENCY = np.pi
+PHASE_SHIFT = 0
 AMPLITUDE = np.pi * np.abs(1 / np.log(3) - 1 / np.log(np.pi))
-FREQUENCY = np.abs(1 / np.log(3) - 1 / np.log(np.pi))
-PHASE_SHIFT = 0.0
-SMOOTHING_SIGMA = 50
-TOLERANCE = 0.875
+ENVELOPE_FREQUENCY = 0.5
+TOLERANCE = 0.01
+
+# t-value generation
+MIN_T = 14.0
+MAX_T = 100000.0
+NUM_POINTS = 100000
