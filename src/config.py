dba49a293017805cf0cda31f535de9869a917b87
S. Sherratt <sadie.sherratt@gmail.com>
2025-05-09 20:54:35 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,3 +2,10 @@ DELTA_CURVE_FILE = "../data/delta_curve.npy"
 DYNAMIC_SINE_ENVELOPE_FILE = "../data/dynamic_sine_envelope.npy"
 WITHIN_BAND_MASK_FILE = "../data/within_band_mask.npy"
 KNOWN_ZEROS_FILE = "../data/zeta_zeros.npy"
+
+# Sieve Parameters
+AMPLITUDE = np.pi * np.abs(1 / np.log(3) - 1 / np.log(np.pi))
+FREQUENCY = np.abs(1 / np.log(3) - 1 / np.log(np.pi))
+PHASE_SHIFT = 0.0
+SMOOTHING_SIGMA = 50
+TOLERANCE = 0.875
