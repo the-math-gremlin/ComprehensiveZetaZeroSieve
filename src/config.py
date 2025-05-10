e712e4a0eae095c328771167a0efa087c5f3a920
S. Sherratt <sadie.sherratt@gmail.com>
2025-05-09 23:10:24 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ AMPLITUDE = np.pi * abs(1 / np.log(3) - 1 / np.log(np.pi)) / 2
 BASE_FREQUENCY = abs(1 / np.log(3) - 1 / np.log(np.pi))
 SMOOTHING_SIGMA = 5.0
 TOLERANCE = 0.875
-PHASE_SHIFT = 0.0
+PHASE_SHIFT = 0.5
 
 # t-value generation
-MIN_T = 0  # Start at the first known zero
+MIN_T = 0  
 MAX_T = 100000.0   # Adjust as needed
 NUM_POINTS = 100000
