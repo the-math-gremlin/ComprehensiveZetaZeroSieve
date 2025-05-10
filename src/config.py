73d195730ebc3eb1281090671c14ae4946c81505
S. Sherratt <sadie.sherratt@gmail.com>
2025-05-10 00:34:38 -0500

    Update config.py
    maybe this time

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-AMPLITUDE = np.pi * abs(1 / np.log(3) - 1 / np.log(np.pi)) / 2  # Theoretical starting point
+# Core sieve parameters
+AMPLITUDE = np.pi * abs(1 / np.log(3) - 1 / np.log(np.pi)) / 2
 BASE_FREQUENCY = abs(1 / np.log(3) - 1 / np.log(np.pi))
 SMOOTHING_SIGMA = 5.0
 TOLERANCE = 0.875
@@ -10,3 +11,6 @@ PHASE_SHIFT = 0.0
 MIN_T = 0.0
 MAX_T = 100000.0
 NUM_POINTS = 100000
+
+# Envelope scaling (dynamic adjustment)
+ENVELOPE_SCALING_FACTOR = 1.5
