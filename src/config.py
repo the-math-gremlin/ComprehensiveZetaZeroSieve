1e81c3f4ecd6333ebca792b3ae9b3171c4e95c61
S. Sherratt <sadie.sherratt@gmail.com>
2025-05-09 20:37:52 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 DELTA_CURVE_FILE = "../data/delta_curve.npy"
 DYNAMIC_SINE_ENVELOPE_FILE = "../data/dynamic_sine_envelope.npy"
 WITHIN_BAND_MASK_FILE = "../data/within_band_mask.npy"
+KNOWN_ZEROS_FILE = "../data/zeta_zeros.npy"
