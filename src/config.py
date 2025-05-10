6e7a0774561141fbdae0d372ad42c233fd0e06db
S. Sherratt <sadie.sherratt@gmail.com>
2025-05-10 00:15:06 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,6 +8,6 @@ TOLERANCE = 0.875
 PHASE_SHIFT = 0.0
 
 # t-value generation
-MIN_T = 10  # Start at the first known zero
+MIN_T = 0  # Start at the first known zero
 MAX_T = 100000.0   # Adjust as needed
 NUM_POINTS = 100000
